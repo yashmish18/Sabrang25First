ea240b895076157d675f9fffb820f69b814d1ba8
yashmish18 <yashmish10930@gmail.com>
2025-06-16 20:55:46 +0530

    Fix: Corrected import paths and adjusted card spacing

--- components/infinite-moving-images.tsx
+++ components/infinite-moving-images.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { cn } from "@/lib/utils";
+import { cn } from "../lib/utils";
 import React, { useEffect, useState } from "react";
 import Image from 'next/image';
 
--- src/app/Events/EventCard.tsx
+++ src/app/Events/EventCard.tsx
@@ -19,7 +19,7 @@ export default function EventCard({ event }: EventProps) {
 
   return (
     <CardContainer className="inter-var">
-      <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
+      <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] h-auto rounded-xl p-6 border">
         <CardItem
           translateZ={50}
           className="text-xl font-bold text-neutral-600 dark:text-white"
