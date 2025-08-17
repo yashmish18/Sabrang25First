e1dab69c881daf04b9880cc023b850c4d771cd2e
Aman Pratap Singh <amanpratapsingh@jklu.edu.in>
2025-08-18 01:50:36 +0530

    Remove unused Spline code from layout.tsx - Clean up layout by removing Spline preloading and caching - Improve performance by eliminating unnecessary network requests

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,27 +23,6 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700;900&family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet" />
-        
-        {/* Essential preloading for Spline */}
-        <link rel="preload" href="https://prod.spline.design/3O0nwQNm6dcILIOA/scene.splinecode" as="fetch" crossOrigin="anonymous" />
-        <link rel="dns-prefetch" href="https://prod.spline.design" />
-        <link rel="preconnect" href="https://prod.spline.design" crossOrigin="anonymous" />
-        
-        {/* Simple preloading script */}
-        <script dangerouslySetInnerHTML={{
-          __html: `
-            // Simple preloading - no complex logic
-            fetch('https://prod.spline.design/3O0nwQNm6dcILIOA/scene.splinecode', {
-              method: 'GET',
-              mode: 'cors',
-              cache: 'force-cache'
-            }).then(res => res.arrayBuffer()).then(buffer => {
-              try {
-                localStorage.setItem('splineSceneCache', JSON.stringify(Array.from(new Uint8Array(buffer))));
-              } catch (e) {}
-            }).catch(() => {});
-          `
-        }} />
       </head>
       <body className="antialiased">
         <VideoProvider>
